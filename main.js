89505f7d193004a7785a0431ea0013d19647b493
alsoeasy <1261396459@qq.com>
2021-07-29 21:47:09 +0800

    设置全局播放器

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App'
 import {toDatabase} from "api/mytool.js";
 
+const MusicPlayer = uni.createInnerAudioContext()
+
+
 Vue.config.productionTip = false
 // 开启调试模式
 Vue.config.devtools = true
-// 添加全局方法
+// 添加全局对象
 Vue.prototype.$database = toDatabase
+Vue.prototype.$audio = MusicPlayer
 
 App.mpType = 'app'
 
